6dc09f76a7d72d0e620453c47ccc8fe2ab9c6219
Ji Chang <mrchangji@outlook.com>
2021-12-30 09:09:23 +0800

    update types

--- packages/archive/scripts/compile.sh
+++ packages/archive/scripts/compile.sh
@@ -1,6 +1,6 @@
 emcc ./main.c /opt/xz-5.2.5/liblzma.a /opt/libarchive-3.5.2/.libs/libarchive.a /opt/openssl-OpenSSL_1_1_1m/libssl.a /opt/openssl-OpenSSL_1_1_1m/libcrypto.a -I /usr/local/include/ -I /opt/libarchive-3.5.2/libarchive\
     -o ./output/libarchive.js \
     -s USE_ZLIB=1 -s USE_BZIP2=1 -s MODULARIZE=1 -s EXPORT_ES6=1 -s EXPORT_NAME=libarchive -s WASM=1 -O3 -s ALLOW_MEMORY_GROWTH=1 \
-    -s EXPORTED_RUNTIME_METHODS='["cwrap","allocate","intArrayFromString"]' -s EXPORTED_FUNCTIONS=@$PWD/libarchive.exports -s ERROR_ON_UNDEFINED_SYMBOLS=0
+    -s EXPORTED_RUNTIME_METHODS='["cwrap","ccall"]' -s EXPORTED_FUNCTIONS=@$PWD/libarchive.exports -s ERROR_ON_UNDEFINED_SYMBOLS=0
 
 echo Done
--- packages/archive/src/libarchive.d.ts
+++ packages/archive/src/libarchive.d.ts
@@ -1,20 +1,72 @@
 import type { EmscriptenModuleFactory, EmscriptenModule } from "emscripten";
 
+type JavaScriptTypeName = "string" | "number";
+
+type JavaScriptType<S extends string | null> = S extends null
+  ? null
+  : S extends "string"
+  ? string
+  : S extends "number"
+  ? number
+  : S extends `string | ${infer R}`
+  ? string | JavaScriptType<R>
+  : S extends `number | ${infer R}`
+  ? number | JavaScriptType<R>
+  : S extends `null | ${infer R}`
+  ? null | JavaScriptType<R>
+  : void;
+
+type JavaScriptTypeArray<T extends JavaScriptTypeName[]> = T extends readonly [
+  infer S,
+  ...infer R
+]
+  ? [JavaScriptType<S>, ...JavaScriptTypeArray<R>]
+  : unknown;
+
+type ModuleReturnType<R extends JavaScriptTypeName> = JavaScriptType<R>;
+type ModuleArgsType<U extends JavaScriptTypeName[]> = JavaScriptTypeArray<U>;
+
 export interface ArchiveModule extends EmscriptenModule {
-  _get_version: () => string;
-  _archive_open: () => void;
-  _get_next_entry: () => void;
-  _get_filedata: () => void;
-  _archive_close: () => void;
-  _archive_entry_filetype: () => void;
-  _archive_entry_pathname: () => void;
-  _archive_entry_pathname_utf8: () => void;
-  _archive_entry_size: () => void;
-  _archive_read_data_skip: () => void;
-  _archive_error_string: () => void;
-  _archive_entry_is_encrypted: () => void;
-  _archive_read_has_encrypted_entries: () => void;
-  _archive_read_add_passphrase: () => void;
+  _cwrap<R extends JavaScriptTypeName, U extends JavaScriptTypeName[]>(
+    fun: string,
+    returnType: R,
+    argTypes: U
+  ): (...args: ModuleArgsType<U>) => ModuleReturnType<R>;
+
+  HEAP8: Int8Array;
+
+  _malloc(size: number): number;
+  _free(ptr: number): void;
+
+  /*
+   * For reference, don't call these functions directly
+   */
+  _get_version(): string;
+  _archive_open(ptr: number, len: number, passphrase: string | null): number;
+  _get_next_entry(archive: number): number;
+  _get_filedata(archive: number, size: number): number;
+  _archive_close(archive: number): void;
+  _archive_entry_filetype(archive: number): number;
+  _archive_entry_pathname(archive: number): string;
+  _archive_entry_pathname_utf8(archive: number): string;
+  _archive_entry_size(archive: number): number;
+  _archive_read_data_skip(archive: number): number;
+  _archive_error_string(archive: number): string;
+  _archive_entry_is_encrypted(archive: number): number;
+  _archive_read_has_encrypted_entries(archive: number): number;
+  _archive_read_add_passphrase(archive: number, passphrase: string): number;
+}
+
+export function wrap(module: ArchiveModule) {
+  return {
+    open: module._cwrap("_archive_open", "number", [
+      "number",
+      "number",
+      "string",
+    ]),
+    close: module._cwrap("_archive_close", null, ["number"]),
+    getNextEntry: module._cwrap("_get_next_entry", "number", ["number"]),
+  };
 }
 
 const libarchive: EmscriptenModuleFactory<ArchiveModule>;
